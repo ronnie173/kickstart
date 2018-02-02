4d2f0a1c02dbbfe7c26112f9564036391b85342e
Jerome Raymond <ronnie173@gmail.com>
2018-02-01 18:59:36 -0600

    169

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,29 +1,39 @@
 import React, {Component} from 'react';
 import Layout from '../../components/Layout';
-import {Form, Button, Input} from 'semantic-ui-react'
+import {Form, Button, Input,Message} from 'semantic-ui-react'
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
     state = {
-        minimumContribution: ''
+        minimumContribution: '',
+        errorMesage:''
+
     };
 
     onSubmit = async (event) => {
         event.preventDefault();
-        const accounts = await web3.eth.getAccounts();
-        await factory.methods
-            .createCampaign(this.state.minimumContribution)
-            .send({
-                from: accounts[0]
+
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await factory.methods
+                .createCampaign(this.state.minimumContribution)
+                .send({
+                    from: accounts[0]
+                })
+        }catch (err){
+            this.setState({
+                errorMessage: err.message
             })
+        }
+
     };
 
     render() {
         return (
             <Layout>
                 <h3>Create a Campaign</h3>
-                <Form onSubmit={this.onSubmit}>
+                <Form onSubmit={this.onSubmit} error={!!this.state.errorMesage}>
                     <Form.Field>
                         <label>Minimum Contribution</label>
                         <Input label="wei"
